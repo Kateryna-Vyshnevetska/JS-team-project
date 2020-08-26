5b7357d00583ce4e13c4a6785df1c2d4205e4ee8
kharchenkok <k.v.kharchenko@ukr.net>
2020-08-26 22:54:21 +0300

    modal data add

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,22 +1,35 @@
-import * as basicLightbox from 'basiclightbox'
+import * as basicLightbox from "basiclightbox";
 import "basiclightbox/dist/basicLightbox.min.css";
 
-import filmCardTpl from '../template/film-card.hbs'
-const mainFilmList = document.querySelector('.list-film')
+import filmCardTpl from "../template/film-card.hbs";
+import { pullData } from "./services/services";
+const mainFilmList = document.querySelector(".list-film");
 
-
-export function openModal(event){
-    console.log(event.target.nodeName);
-    // if(event.target.nodeName !== 'IMG'){
-    //     return
-    // }else{
-    //     const instance = basicLightbox.create(
-       //     instance.show()
-
-    // }
-    let markup = filmCardTpl()
-    const instance = basicLightbox.create(markup)
-    instance.show()
+export function openModal(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  } else {
+    let id = event.target.dataset.id;
+    getCurrentObj(id);
+    // document.body.classList.add('modal-open')
+  }
 }
 
-mainFilmList.addEventListener('click', openModal)
+function getCurrentObj(id) {
+  const getInfo = pullData();
+  let currentObj;
+  getInfo.forEach((elem) => {
+    if (elem.id === Number(id)) {
+      currentObj = elem;
+    }
+  });
+  drawModal(currentObj);
+}
+
+function drawModal(obj) {
+    let markup = filmCardTpl(obj);
+    const instance = basicLightbox.create(markup);
+    instance.show();
+}
+
+mainFilmList.addEventListener("click", openModal);
