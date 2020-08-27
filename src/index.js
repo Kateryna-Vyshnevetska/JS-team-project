19c3f45afadec322865a7e3e9664b0d84845ec9c
Peter Ivanovskij <65976810+Ivanovskij-Peter@users.noreply.github.com>
2020-08-27 13:11:28 +0300

    добавил поиск по id

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import "./components/services/services.js";
 import "./components/search.js";
 import "./components/loader.js";
 import "./components/header-nav-btn.js";
-import "./components/paginator.js"
-import "./components/localStorage.js"
-
+import "./components/paginator.js";
+import "./components/localStorage.js";
+import "./components/trailer.js";
