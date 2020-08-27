cb9a8c080692257c106c20ca74ead2ae0eeb3b90
andrew65215584 <63922036+andrew65215584@users.noreply.github.com>
2020-08-27 18:12:23 +0300

    сделал карусель на сайте со стилями

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./components/modal";
 import "./styles.css";
 import "./mainStyle.css";
+import "./slider.css";
 import "./components/services/services.js";
 import "./components/search.js";
 import "./components/loader.js";
@@ -8,4 +9,4 @@ import "./components/header-nav-btn.js";
 import "./components/paginator.js"
 import "./components/localStorage.js"
 import "./components/sortByPopularity.js"
-
+import "./components/carousel.js"
