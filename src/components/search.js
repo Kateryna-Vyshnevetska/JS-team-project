79d2140431a305e3c617dc040f9b0c7627896c6c
Tofik <smilokiller@mail.ru>
2020-08-29 13:02:28 +0300

    all done:)

--- src/components/search.js
+++ src/components/search.js
@@ -7,53 +7,55 @@ import { getGenres } from "./services/services.js";
 import { drawHtml } from "./services/services.js";
 import { sortByPopularity } from "./sortByPopularity.js";
 
-export const checkInput = function (e) {
-  e.preventDefault();
+export const checkInput = function(e) {
+    e.preventDefault();
 
-  let reg = /[^\d\sA-Z]/gi;
-  let inputValue = e.target.value.match(reg);
+    let reg = /[^\d\sA-Z]/gi;
+    let inputValue = e.target.value.match(reg);
 
-  const d = filmsSearch(e.target.value).then((f) => {
-    return getGenres().then((g) =>
-      f.map((el) => ({
-        ...el,
-        genre_ids: el.genre_ids.flatMap((num) =>
-          g.filter((el) => el.id === num)
-        ),
-      }))
-    );
-  });
-  d.then(drawHtml);
+    const d = filmsSearch(e.target.value).then((f) => {
+        return getGenres().then((g) =>
+            f.map((el) => ({
+                ...el,
+                genre_ids: el.genre_ids.flatMap((num) =>
+                    g.filter((el) => el.id === num)
+                ),
+            })).sort(
+                (a, b) => b.vote_average - a.vote_average
+            )
+        );
 
-  if (!inputValue) {
-    setTimeout(() => {
-      refs.notFoundContainer.classList.add("is-not-visible");
-      refs.searchInfo.classList.remove("unSuccessful");
-      refs.searchInfo.classList.add("successful");
-      refs.searchInfo.style.textAlign = "left";
-      refs.searchInfo.textContent = `Found ${totalResults} movie(s) by your request`;
-      sortByPopularity();
-      if (totalResults === 0) {
-        refs.notFoundContainer.classList.remove("is-not-visible");
-        getPopular();
+    });
+    d.then(console.log()).then(drawHtml);
+
+    if (!inputValue) {
+        setTimeout(() => {
+            refs.notFoundContainer.classList.add("is-not-visible");
+            refs.searchInfo.classList.remove("unSuccessful");
+            refs.searchInfo.classList.add("successful");
+            refs.searchInfo.style.textAlign = "left";
+            refs.searchInfo.textContent = `Found ${totalResults} movie(s) by your request`;
+            if (totalResults === 0) {
+                refs.notFoundContainer.classList.remove("is-not-visible");
+                getPopular();
+                refs.searchInfo.classList.remove("successful");
+                refs.searchInfo.classList.add("unSuccessful");
+                refs.searchInfo.textContent = `Found ${totalResults} movie(s) by your request`;
+                refs.paginationRef.classList.add("is-not-visible");
+            } else if (e.target.value === "") {
+                getPopular();
+                refs.searchInfo.textContent = "";
+                refs.notFoundContainer.classList.add("is-not-visible");
+            }
+        }, 800);
+    } else {
         refs.searchInfo.classList.remove("successful");
         refs.searchInfo.classList.add("unSuccessful");
-        refs.searchInfo.textContent = `Found ${totalResults} movie(s) by your request`;
+        refs.searchInfo.textContent = `Search result not successful. Enter the correct movie name and try again`;
         refs.paginationRef.classList.add("is-not-visible");
-      } else if (e.target.value === "") {
-        getPopular();
-        refs.searchInfo.textContent = "";
+        refs.searchInfo.style.textAlign = "center";
         refs.notFoundContainer.classList.add("is-not-visible");
-      }
-    }, 800);
-  } else {
-    refs.searchInfo.classList.remove("successful");
-    refs.searchInfo.classList.add("unSuccessful");
-    refs.searchInfo.textContent = `Search result not successful. Enter the correct movie name and try again`;
-    refs.paginationRef.classList.add("is-not-visible");
-    refs.searchInfo.style.textAlign = "center";
-    refs.notFoundContainer.classList.add("is-not-visible");
-  }
+    }
 };
 
 refs.inputSearchRef.addEventListener("change", debounce(checkInput, 1000));
